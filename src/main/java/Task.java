892d1060702609424a6ea94f66f1c8e6a30c84f6
IZO-Ong <ong.izo.zh@gmail.com>
2026-01-16 10:22:36 +0800

    Add Task Class

---
+++ src/main/java/Task.java
@@ -0,0 +1,29 @@
+public class Task {
+    private final String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        isDone = false;
+    }
+
+    public String markDone() {
+        if (!isDone) {
+            isDone = true;
+            return "I've marked this task as done:\n  " + getCheckboxIcon() + description;
+        }
+        return "This task is already marked as done!";
+    }
+
+    private String getCheckboxIcon() {
+        return "[" + (isDone ? "X" : " ") + "]";
+    }
+
+    public String markUndone() {
+        if (isDone) {
+            isDone = false;
+            return "OK, I've marked this task as not done yet:\n  " + getCheckboxIcon() + description;
+        }
+        return "This task is already not done!";
+    }
+}
