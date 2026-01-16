1ee4290556d088c53f6c7a13bdd1e68f818aa2b0
IZO-Ong <ong.izo.zh@gmail.com>
2026-01-16 09:02:55 +0800

    Add echo feature, and add indentation to Sappy's text

--- src/main/java/Sappy.java
+++ src/main/java/Sappy.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
+
 public class Sappy {
 
     public static void pprint(String text) {
-        System.out.println("____________________________________________________________");
-        System.out.println(text);
-        System.out.println("____________________________________________________________");
+        String indent = "    ";
+        String line = "____________________________________________________________";
+
+        System.out.println(indent + line);
+
+        String indentedText = indent + text.replace("\n", "\n" + indent);
+        System.out.println(indentedText);
+
+        System.out.println(indent + line);
     }
 
     public static void main(String[] args) {
+        Scanner myScan = new Scanner(System.in);
         pprint("Hello! I'm Sappy \n" +
                 "What can I do for you?");
 
-        pprint("Bye. Hope to see you again soon!");
+        String userInput = myScan.nextLine();
+        while (!userInput.equalsIgnoreCase("bye")) {
+            pprint(userInput);
+            userInput = myScan.nextLine();
+        }
+        pprint("Bye! Sappy will be very lonely until you come back!");
     }
 }
