affbd59fabbb218d61888a54d95af639646de7e9
IZO-Ong <ong.izo.zh@gmail.com>
2026-01-16 08:55:37 +0800

    Rename all instance of Duke to Sappy, add greeting

---
+++ src/main/java/Sappy.java
@@ -0,0 +1,15 @@
+public class Sappy {
+
+    public static void pprint(String text) {
+        System.out.println("____________________________________________________________");
+        System.out.println(text);
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        pprint("Hello! I'm Sappy \n" +
+                "What can I do for you?");
+
+        pprint("Bye. Hope to see you again soon!");
+    }
+}
