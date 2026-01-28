a9a5eabf0bde5900ba5aaf82e9a38509a71a5b50
IZO-Ong <ong.izo.zh@gmail.com>
2026-01-28 11:31:02 +0800

    Add images and basic GUI

---
+++ src/main/java/sappy/DialogBox.java
@@ -0,0 +1,34 @@
+package sappy;
+
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+/**
+ * An immutable custom control using HBox to represent a dialog box in the GUI.
+ * It consists of an ImageView to represent the speaker's face and a label
+ * containing text from the speaker.
+ */
+public class DialogBox extends HBox {
+    private Label text;
+    private ImageView displayPicture;
+
+    /**
+     * Constructs a new DialogBox with the specified text and image.
+     * @param s The string content to be displayed in the dialog box.
+     * @param i The image to be displayed as the speaker's avatar.
+     */
+    public DialogBox(String s, Image i) {
+        text = new Label(s);
+        displayPicture = new ImageView(i);
+
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+
+        this.setAlignment(Pos.TOP_RIGHT);
+        this.getChildren().addAll(text, displayPicture);
+    }
+}
